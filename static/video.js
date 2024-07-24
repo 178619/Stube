cfe619b308ce27773f1216bcbf38e23ba7691a2c
Fryze <snoo91919@gmail.com>
2024-07-25 05:19:12 +0900

    menus & svg

--- static/video.js
+++ static/video.js
@@ -18,7 +18,38 @@ const init = () => {
         if (oneAlertHandler[0]) oneAlertHandler[0].remove()
         oneAlertHandler.shift()
     }
-
+    const openMenu = (t, v) => {
+        const d = document.createElement('div')
+        d.classList.add('menu')
+        const head = document.createElement('div')
+        head.classList.add('head')
+        head.innerText = t
+        const closer = document.createElement('button')
+        closer.classList.add('closer')
+        closer.onclick = (e) => {
+            if (e.pointerType == 'mouse' && e.button != 0) return
+            removeMenus()
+        }
+        head.appendChild(closer)
+        d.appendChild(head)
+        d.appendChild(v)
+        document.getElementById("mask").appendChild(d)
+        return d
+    }
+    const setMenuHandler = []
+    const setMenu = (t, v) => {
+        if (setMenuHandler.length) {
+            setMenuHandler[0].remove()
+            setMenuHandler.shift()
+        }
+        setMenuHandler.push(openMenu(t, v))
+    }
+    const removeMenus = () => {
+        while (setMenuHandler.length) {
+            setMenuHandler[0].remove()
+            setMenuHandler.shift()
+        }
+    }
     let pointerWaitTime = 0
     const keyList = [
         " ", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown", ",", ".", ">", "<", "Home", "End",
@@ -68,6 +99,9 @@ const init = () => {
         video.currentTime = q
         oneAlert('To: '+getTimeString(video.currentTime))
     }
+
+    const locationOptions = Object.fromEntries(location.search.slice(1).split('&').filter((v)=>{return v.length}).map((v)=>{return [v.split('=')[0], v.split('=').slice(1).join('=')]}))
+
     document.body.onkeydown = (e) => {
         if (!keyList.includes(e.key) || e.target.id == 'search' || e.ctrlKey) return
         e.preventDefault()
@@ -148,8 +182,8 @@ const init = () => {
         }
     }
     mask.onpointerdown = (e) => {
-        if (e.target.id != 'mask') return
-        if (e.pointerType == "touch") {
+        if (e.target.id != 'mask' || e.pointerType == 'mouse' && e.button != 0) return
+        if (e.pointerType == 'touch') {
             if (mask.classList.toString().includes('shown')) {
                 mask.classList.remove('shown')
             } else {
@@ -185,7 +219,13 @@ const init = () => {
             updateSeeker()
         }
     }
-    document.getElementById('center').onpointerdown = () => {
+    document.querySelector('#mask h1').onpointerdown = (e) => {
+        if (e.pointerType == 'mouse' && e.button != 0) return
+        if (!document.querySelector('.embed')) return
+        open(location.origin + location.pathname.replace('/e/', '/v/'))
+    }
+    document.getElementById('center').onpointerdown = (e) => {
+        if (e.pointerType == 'mouse' && e.button != 0) return
         playOrPause()
         document.getElementById('center').style.transform = 'scale(0.875)'
     }
@@ -196,6 +236,7 @@ const init = () => {
         document.getElementById('center').style.transform = null
     }
     document.getElementById('seeker').onwheel = (e) => {
+        if (e.ctrlKey) return
         e.preventDefault()
         addTime(e.wheelDeltaY / 100)
     }
@@ -203,22 +244,28 @@ const init = () => {
         video.currentTime = document.getElementById('seeker').value / 1000
         oneAlert('To: '+getTimeString(video.currentTime))
     }
-    document.getElementById('play-pause').onmousedown = playOrPause
-    document.getElementById('loop').onmousedown = () => {
+    document.getElementById('play-pause').onpointerdown = (e) => {
+        if (e.pointerType == 'mouse' && e.button != 0) return
+        playOrPause()
+    }
+    document.getElementById('loop').onpointerdown = (e) => {
+        if (e.pointerType == 'mouse' && e.button != 0) return
         video.loop = !video.loop
         if (!video.loop) {
-            document.getElementById("loop").style.backgroundImage = 'url(/static/icons/repeat-off.png)'
+            document.getElementById("loop").style.backgroundImage = 'url(/static/icons/repeat-off.svg)'
             oneAlert('Loop Turned Off')
         } else if (true) {
-            document.getElementById("loop").style.backgroundImage = 'url(/static/icons/repeat-once.png)'
+            document.getElementById("loop").style.backgroundImage = 'url(/static/icons/repeat-once.svg)'
             oneAlert('Loop Turned On')
         }
     }
-    document.getElementById('volume-icon').onmousedown = () => {
+    document.getElementById('volume-icon').onpointerdown = (e) => {
+        if (e.pointerType == 'mouse' && e.button != 0) return
         video.muted = !video.muted
         updateVolume()
     }
     document.getElementById('volume-icon').onwheel = (e) => {
+        if (e.ctrlKey) return
         e.preventDefault()
         if (e.deltaY < 0) {
             if (video.volume < 0.95) video.volume = Math.round(video.volume * 20 + 1) / 20; else video.volume = 1;
@@ -234,6 +281,7 @@ const init = () => {
         updateVolume()
     }
     document.getElementById('volume').onwheel = (e) => {
+        if (e.ctrlKey) return
         e.preventDefault()
         if (e.deltaY < 0) {
             if (video.volume < 0.95) video.volume = Math.round(video.volume * 20 + 1) / 20; else video.volume = 1;
@@ -243,31 +291,38 @@ const init = () => {
         updateVolume()
     }
     if (!video.textTracks.length) document.getElementById('captions').style.display = 'none'
-    document.getElementById('captions').onmousedown = () => {
+    document.getElementById('captions').onpointerdown = (e) => {
+        if (e.pointerType == 'mouse' && e.button != 0) return
         const tracks = Array.from(video.textTracks)
-        const index = tracks.findIndex((v)=>{return v.mode=="showing"})
-        let languageName
-        try {
-            if (index+1 != tracks.length) languageName = languageNames.of(tracks[index+1].language)
-        } catch {
-            languageName = tracks[index+1].language
-        }
-        if (index == -1) {
-            tracks[0].mode = 'showing'
-            oneAlert(tracks[0].kind.slice(0,1).toUpperCase()+tracks[0].kind.slice(1)+': '+languageName)
-        } else {
-            tracks[index].mode = 'disabled'
-            if (index+1 != tracks.length) {
-                tracks[index+1].mode = 'showing'
-                oneAlert(tracks[index+1].kind.slice(0,1).toUpperCase()+tracks[index+1].kind.slice(1)+': '+languageName)
-            } else oneAlert('Captions: Off')
-        }
+        const ul = document.createElement('ul')
+        tracks.forEach((v)=>{
+            const li = document.createElement('li')
+            try {
+                li.innerText = languageNames.of(v.language)
+            } catch {
+                li.innerText = v.language
+            }
+            if (v.mode == 'showing') li.classList.add('enabled')
+            li.onpointerdown = (e) => {
+                if (e.pointerType == 'mouse' && e.button != 0) return
+                if (v.mode == 'showing') {
+                    v.mode = 'disabled'
+                    li.classList.remove('enabled')
+                } else {
+                    v.mode = 'showing'
+                    li.classList.add('enabled')
+                }
+            }
+            ul.appendChild(li)
+        })
+        setMenu('Captions', ul)
     }
-    document.getElementById('screenshot').onmousedown = () => {
+    document.getElementById('screenshot').onpointerdown = (e) => {
+        if (e.pointerType == 'mouse' && e.button != 0) return
         if (!video.videoWidth || !video.videoHeight) {
             const a = document.createElement('a')
             a.href = '/t/' + location.pathname.slice(3)
-            a.download = decodeURIComponent(location.pathname.split('/').pop())+'.png'
+            a.download = decodeURIComponent(location.pathname.split('/').pop())+'.svg'
             document.body.appendChild(a)
             a.click()
             a.remove()
@@ -281,17 +336,30 @@ const init = () => {
         ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight)
         const a = document.createElement('a')
         a.href = canvas.toDataURL('image/png')
-        a.download = decodeURIComponent(location.pathname.split('/').pop())+'-'+getTimeString(video.currentTime)+(video.currentTime%1+'').replace('.',':').slice(1,5)+'.png'
+        a.download = decodeURIComponent(location.pathname.split('/').pop())+'-'+getTimeString(video.currentTime)+(video.currentTime%1+'').replace('.',':').slice(1,5)+'.svg'
         document.body.appendChild(a)
         a.click()
         a.remove()
         oneAlert('Screenshot Taken')
     }
-    document.getElementById('playspeed').onmousedown = () => {
-        if (video.playbackRate < 2) video.playbackRate = Math.round(video.playbackRate * 4 + 1) / 4; else video.playbackRate = 0.25;
-        oneAlert('Playspeed: ' + video.playbackRate + 'x')
+    const speedList = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 2, 2.5, 3, 4]
+    document.getElementById('playspeed').onpointerdown = (e) => {
+        if (e.pointerType == 'mouse' && e.button != 0) return
+        const ul = document.createElement('ul')
+        speedList.forEach((v)=>{
+            const li = document.createElement('li')
+            li.innerText = v + 'x'
+            li.onpointerdown = (e) => {
+                if (e.pointerType == 'mouse' && e.button != 0) return
+                video.playbackRate = v
+                oneAlert('Playspeed: ' + video.playbackRate + 'x')
+            }
+            ul.appendChild(li)
+        })
+        setMenu('Playspeed', ul)
     }
     document.getElementById('playspeed').onwheel = (e) => {
+        if (e.ctrlKey) return
         e.preventDefault()
         if (e.deltaY < 0) {
             if (video.playbackRate < 3.75) video.playbackRate = Math.round(video.playbackRate * 4 + 1) / 4; else video.playbackRate = 4;
@@ -302,22 +370,48 @@ const init = () => {
         document.getElementById('playspeed').style.transform = 'scale(0.875)'
         setTimeout(()=>{document.getElementById('playspeed').style.transform = null}, 100)
     }
-    document.getElementById('embedlink').onmousedown = () => {
-        const a = document.createElement('a')
-        a.href = document.getElementById('embedlink').getAttribute('href')
-        a.target = '_blank'
-        document.body.appendChild(a)
-        a.click()
-        a.remove()
+    document.getElementById('embedlink').onpointerdown = (e) => {
+        if (e.pointerType == 'mouse' && e.button == 1) {
+            open(location.origin + location.pathname.replace('/v/', '/e/'))
+            return
+        }
+        if (e.pointerType == 'mouse' && e.button != 0) return
+        navigator.clipboard.writeText('<iframe src="'+location.origin+location.pathname.replace('/v/', '/e/')+'" width=320 height=180 allowfullscreen></iframe>')
+        oneAlert('HTML code Copied to Clipboard.')
     }
-    document.getElementById('filelink').onmousedown = () => {
+    document.getElementById('filelink').onpointerdown = (e) => {
+        if (e.pointerType == 'mouse' && e.button != 0) return
         const a = document.createElement('a')
         a.href = document.getElementById('filelink').getAttribute('href')
         document.body.appendChild(a)
         a.click()
         a.remove()
     }
-    document.getElementById('fullscreen').onmousedown = getFullscreen
+    document.getElementById('current').style.display = 'block';
+    document.getElementById('collapse').onpointerdown = (e) => {
+        if (e.pointerType == 'mouse' && e.button != 0) return
+        if (document.getElementById('current').style.display != 'block') {
+            document.getElementById('current').style.display = 'block';
+            document.getElementById('captions').style.display = '';
+            document.getElementById('screenshot').style.display = '';
+            document.getElementById('playspeed').style.display = '';
+            document.getElementById('embedlink').style.display = '';
+            document.getElementById('filelink').style.display = '';
+            document.getElementById("collapse").style.backgroundImage = 'url(/static/icons/tune.svg)'
+        } else {
+            document.getElementById('current').style.display = '';
+            document.getElementById('captions').style.display = 'block';
+            document.getElementById('screenshot').style.display = 'block';
+            document.getElementById('playspeed').style.display = 'block';
+            document.getElementById('embedlink').style.display = 'block';
+            document.getElementById('filelink').style.display = 'block';
+            document.getElementById("collapse").style.backgroundImage = 'url(/static/icons/tune-vertical.svg)'
+        }
+    }
+    document.getElementById('fullscreen').onpointerdown = (e) => {
+        if (e.pointerType == 'mouse' && e.button != 0) return
+        getFullscreen()
+    }
     document.body.onfullscreenchange = (e) => {
         if (document.fullscreenElement) {
             player.classList.add('fullscreen')
@@ -340,27 +434,27 @@ const init = () => {
         document.getElementById("seeker").style.backgroundImage = `linear-gradient(to right, rgba(0,0,0,0) 6px, var(--main-color) 6px, var(--main-color) calc(6px + ${ct * 100}% - 12px * ${ct}), rgba(223,223,223,0.75) calc(6px + ${ct * 100}% - 12px * ${ct}), rgba(223,223,223,0.75) calc(6px + ${bt * 100}% - 12px * ${bt}), rgba(191,191,191,0.5) calc(6px + ${bt * 100}% - 12px * ${bt}), rgba(191,191,191,0.5) calc(100% - 6px), rgba(191,191,191,0.5) calc(100% - 6px), rgba(0,0,0,0) calc(100% - 6px))`
         document.getElementById("current").innerText = getTimeString(video.currentTime) + ' / ' + getTimeString(video.duration)
         if (!!(video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2)) {
-            document.getElementById("center").style.backgroundImage = 'url(/static/icons/pause.png)'
-            document.getElementById("play-pause").style.backgroundImage = 'url(/static/icons/pause.png)'
+            document.getElementById("center").style.backgroundImage = 'url(/static/icons/pause.svg)'
+            document.getElementById("play-pause").style.backgroundImage = 'url(/static/icons/pause.svg)'
         } else if (video.currentTime == video.duration) {
-            document.getElementById("center").style.backgroundImage = 'url(/static/icons/replay.png)'
-            document.getElementById("play-pause").style.backgroundImage = 'url(/static/icons/replay.png)'
+            document.getElementById("center").style.backgroundImage = 'url(/static/icons/replay.svg)'
+            document.getElementById("play-pause").style.backgroundImage = 'url(/static/icons/replay.svg)'
         } else {
-            document.getElementById("center").style.backgroundImage = 'url(/static/icons/play.png)'
-            document.getElementById("play-pause").style.backgroundImage = 'url(/static/icons/play.png)'
+            document.getElementById("center").style.backgroundImage = 'url(/static/icons/play.svg)'
+            document.getElementById("play-pause").style.backgroundImage = 'url(/static/icons/play.svg)'
         }
     }
     const updateVolume = () => {
         document.getElementById("volume").style.backgroundImage = `linear-gradient(to right, rgba(0,0,0,0) 6px, var(--main-color) 6px, var(--main-color) calc(6px + ${video.volume * 100}% - 12px * ${video.volume}), rgba(191,191,191,0.5) calc(6px + ${video.volume * 100}% - 12px * ${video.volume}), rgba(191,191,191,0.5) calc(100% - 6px), rgba(0,0,0,0) calc(100% - 6px))`
         document.getElementById("volume").value = video.volume
         if (video.muted) {
-            document.getElementById("volume-icon").style.backgroundImage = 'url(/static/icons/volume-off.png)'
+            document.getElementById("volume-icon").style.backgroundImage = 'url(/static/icons/volume-off.svg)'
         } else if (video.volume == 0) {
-            document.getElementById("volume-icon").style.backgroundImage = 'url(/static/icons/volume-low.png)'
+            document.getElementById("volume-icon").style.backgroundImage = 'url(/static/icons/volume-low.svg)'
         } else if (video.volume < 1/2) {
-            document.getElementById("volume-icon").style.backgroundImage = 'url(/static/icons/volume-medium.png)'
+            document.getElementById("volume-icon").style.backgroundImage = 'url(/static/icons/volume-medium.svg)'
         } else {
-            document.getElementById("volume-icon").style.backgroundImage = 'url(/static/icons/volume-high.png)'
+            document.getElementById("volume-icon").style.backgroundImage = 'url(/static/icons/volume-high.svg)'
         }
         oneAlert('Volume: '+(video.muted ? 'Muted' : Math.floor(video.volume * 100)+'%'))
     }
